860c4e1ea7d488a66b93944ebaf6753e46cb0bb8
VishalTheHuman <vishalatmadurai@outlook.com>
2023-03-28 06:46:39 +0530

    Updated

---
+++ 1952. Three Divisors/isThree.c
@@ -0,0 +1,15 @@
+bool isThree(int n){
+    int k=1;
+    for(int i=1;i<=n/2;i++){
+        if(n%i==0){
+            k++;
+        }
+        if(k>3){
+            return false;
+        }
+    }
+    if(k==3){
+        return true;
+    }
+    return false;
+}
