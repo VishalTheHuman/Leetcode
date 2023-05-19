d2580d912e55b7ea0d1988b9d920ba72e6c9cea9
VishalTheHuman <vishalatmadurai@outlook.com>
2023-05-19 21:43:52 +0530

    Updated

--- 921. Minimum Add to Make Parentheses Valid/Solution.java
+++ 921. Minimum Add to Make Parentheses Valid/Solution.java
@@ -4,18 +4,15 @@ class Solution {
         for(int i =0;i<s.length();i++){
             if(s.charAt(i)=='('){
                 ret.push('(');
-            }else{
-                if(!ret.empty()){
-                    if(ret.peek()=='('){
-                        ret.pop();
-                    }else{
-                        ret.push(')');
-                    }
+            }else if(!ret.empty()){
+                if(ret.peek()=='('){
+                    ret.pop();
                 }else{
                     ret.push(')');
                 }
-                
-            }
+            }else{
+                ret.push(')');
+            }      
         }
         return ret.size();
     }
