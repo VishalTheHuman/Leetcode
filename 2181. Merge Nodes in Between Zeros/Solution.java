11d46a9fa34424411d4b4ace07dbd07939096d2d
VishalTheHuman <vishalatmadurai@outlook.com>
2023-06-20 16:41:12 +0530

    Updated

---
+++ 2181. Merge Nodes in Between Zeros/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        int count = 0;
+        ListNode ret = new ListNode();
+        ListNode temp = new ListNode();
+        ret = temp;
+        while(head!=null){
+            if(head.val==0){
+                if(temp.val==0){
+                    temp.val = count;
+                }else{
+                    ListNode t = new ListNode();
+                    t.val = count;
+                    t.next = null;
+                    temp.next = t;
+                    temp = temp.next;
+                }
+                count=0;
+            }else{
+                count+=head.val;
+            }
+            head = head.next;
+        }
+        return ret;
+    }
+}
