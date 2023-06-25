1ef66294e91471cb33076be7f3fcf5aef9039539
VishalTheHuman <vishalatmadurai@outlook.com>
2023-06-25 07:21:09 +0530

    Updated

---
+++ 2396. Strictly Palindromic Number/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        for(int i=2;i<=n-2;i++){
+            if(!isPalindrome(convertBase(n,i))){
+                return false;
+            }
+        }
+        return true;
+    }
+    public String convertBase(int n,int base){
+        StringBuilder ret = new StringBuilder();
+        while(n>0){
+            int modulo = n%base;
+            n/=base;
+            ret.insert(0,Integer.toString(n));
+        }
+        return ret.toString();
+    }
+    public boolean isPalindrome(String s){
+        int n=s.length();
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(n-1-i)) return false;
+        }
+        return true;
+    }
+}
