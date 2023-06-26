3f82057c85e716e19b0f136a324aa05157f04ee6
VishalTheHuman <vishalatmadurai@outlook.com>
2023-06-26 18:48:03 +0530

    Updated

--- 442. Find All Duplicates in an Array/Solution-HashMap-Worst.java
+++ 442. Find All Duplicates in an Array/Solution-HashMap-Worst.java
@@ -1,17 +1,12 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         HashMap<Integer,Integer> table = new HashMap<>();
+        List<Integer> ret = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             if(table.get(nums[i])==null){
                 table.put(nums[i],1);
             }else{
-                table.put(nums[i],table.get(nums[i])+1);
-            }
-        }
-        List<Integer> ret = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> x : table.entrySet()){
-            if(x.getValue()>1){
-                ret.add(x.getKey());
+                ret.add(nums[i]);
             }
         }
         return ret;
