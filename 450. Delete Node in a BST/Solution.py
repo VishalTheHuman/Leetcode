12ea86a26026113fa408d5122496ce0ba8d2afa5
VishalTheHuman <vishalatmadurai@outlook.com>
2023-09-10 17:27:39 +0530

    Updated

---
+++ 450. Delete Node in a BST/Solution.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if key < root.val:
+            root.left = self.deleteNode(root.left,key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right,key)
+        else:
+            if not root.left and not root.right:
+                return None
+            if root.left is None:
+                return root.right
+            if root.right is None:
+                return root.left
+            
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            min_val = curr.val
+            root.val = min_val
+            root.right = self.deleteNode(root.right,min_val)
+        return root
+        
--- 938. Range Sum of BST/Solution.py
+++ 938. Range Sum of BST/Solution.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         if root:
