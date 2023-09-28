ccf23368eb76274ff62ecbf33714e90ed5547ab1
VishalTheHuman <vishalatmadurai@outlook.com>
2023-09-28 17:41:31 +0530

    Updated

---
+++ 116. Populating Next Right Pointers in Each Node/Solution_2.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        q = deque([root])
+        while q and root:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            j = 1
+            for i in range(len(q)-1):
+                n = q[i]
+                n.next = q[j]
+                j+=1
+        return root
