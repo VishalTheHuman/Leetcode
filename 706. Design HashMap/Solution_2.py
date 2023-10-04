802da48bbc03d1507be7d5f7a3e6965e54755f8c
VishalTheHuman <vishalatmadurai@outlook.com>
2023-10-04 16:35:07 +0530

    Updated

--- 706. Design HashMap/Solution_2.py
+++ 706. Design HashMap/Solution_2.py
@@ -1,18 +1,45 @@
 class MyHashMap:
 
     def __init__(self):
-        self.h = {}
-
+        self.h = [[] for _ in range(100)]
+    
+    def _get_hash(self,val):
+        val = str(val)
+        k = 0
+        for ch in val:
+            k+=ord(ch)
+        return k % 100
+    
     def put(self, key: int, value: int) -> None:
-        self.h[key] = value
-
+        h = self._get_hash(key)
+        i = 0
+        met = False
+        for x in self.h[h]:
+            if x[0]==key:
+                self.h[h].pop(i)
+                self.h[h].insert(i,(key,value))
+                met = True
+                break
+            i+=1
+        if not met:
+            self.h[h].append((key,value))
+        
     def get(self, key: int) -> int:
-        return self.h.get(key,-1)
+        h = self._get_hash(key)
+        for x in self.h[h]:
+            if x[0]==key:
+                return x[1]
+        return -1
 
     def remove(self, key: int) -> None:
-        if self.h.get(key,False):
-            del self.h[key] 
-
+        h = self._get_hash(key)
+        if self.h[h]:
+            i = 0
+            for x in self.h[h]:
+                if x[0]==key:
+                    self.h[h].pop(i)
+                    break
+                i+=1
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
