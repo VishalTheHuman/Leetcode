9f7fe5fd46d0408dcac5e2faf871d5f1051c4bf8
VishalTheHuman <vishalatmadurai@outlook.com>
2023-10-07 07:00:45 +0530

    Updated

---
+++ 513. Find Bottom Left Tree Value/Solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        left_node = q[0]
+        while q and root:
+            left_node = q[0]
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return left_node.val
