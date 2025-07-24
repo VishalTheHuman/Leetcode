d297ec97a3c9701332d98064e11066622c396c3d
Vishal <117697246+VishalTheHuman@users.noreply.github.com>
2025-07-24 21:09:16 +0530

    Updated

---
+++ 81. Search in Rotated Sorted Array II/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        l = 0 
+        r = len(nums) - 1
+        mid = len(nums)//2
+        while l < r and nums[mid] != target: 
+            if nums[l] == nums[r] == nums[mid]: 
+                l+=1
+                r-=1
+                continue
+            if nums[mid] > target: 
+                if nums[l] > nums[mid] or nums[l] <= target: 
+                    r = mid - 1
+                else: 
+                    l = mid + 1
+            else: 
+                if nums[r] < nums[mid] or nums[r] >= target: 
+                    l = mid + 1
+                else: 
+                    r = mid -1 
+            mid = (l+r)//2
+        return nums[mid] == target
