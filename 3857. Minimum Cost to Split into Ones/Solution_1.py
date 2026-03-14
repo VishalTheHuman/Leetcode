08cdbd296643f7f3912e36b08ae0ed98752cfa3d
Vishal <117697246+VishalTheHuman@users.noreply.github.com>
2026-03-14 21:44:59 +0530

    Updated

--- 3857. Minimum Cost to Split into Ones/Solution_1.py
+++ 3857. Minimum Cost to Split into Ones/Solution_1.py
@@ -1,20 +1,3 @@
 class Solution:
     def minCost(self, n: int) -> int:
-        store = defaultdict(int)
-        score = 0 
-
-        def recurse(num): 
-            if num == 1:
-                return 0
-            a = b = 0
-            if store[num] == 0:
-                s = float(inf)
-                for i in range(1,int(math.sqrt(num))+1):
-                    ta, tb = num-i, i
-                    if s > ta*tb : 
-                        a,b = ta,tb
-                        s = ta*tb
-                store[num] = s + recurse(a) + recurse(b)
-            return store[num]
-
-        return recurse(n)
+        return (n*(n-1)) // 2
